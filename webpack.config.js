186442007ef069dafb117079ffa71af37b61ef24
EggsyJ <58285674+JeonChanMin@users.noreply.github.com>
2023-03-21 02:31:55 +0900

    set webpack-config

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    clean: true,
+  },
+
+  mode: "development",
+};
